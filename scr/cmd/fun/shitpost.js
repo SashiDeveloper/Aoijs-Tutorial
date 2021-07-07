691878e284cf8494fe5eef1dbacdd7741e7e6896
SashiDeveloper <83094605+SashiDeveloper@users.noreply.github.com>
2021-07-07 09:49:49 -0300

    Add files via upload

---
+++ scr/cmd/fun/shitpost.js
@@ -0,0 +1,8 @@
+module.exports.command = {
+  name: "shitpost",
+  code: `$title[Shitpost]
+  $thumbnail[$userAvatar[$authorID]]
+  $image[https://p.dandan449.repl.co/shit/$random[1;27]]
+  $addTimesTamp
+  $color[RANDOM]`
+}
